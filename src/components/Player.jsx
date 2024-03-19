fbce1d37bc4c8bdf5ccd8f6697a2716cd971427d
Zakria Mohiyuddin <114757404+Zakkuzakria@users.noreply.github.com>
2024-03-19 12:55:04 +0530

    Add files via upload

---
+++ src/components/Player.jsx
@@ -0,0 +1,23 @@
+import { useRef, useState} from "react";
+
+
+export default function Player() {
+  const playerName = useRef();
+
+  const [enterPlayerName, setEnterPlayerName] = useState(null);
+
+  function handleClick() {
+    setEnterPlayerName(playerName.current.value);
+    playerName.current.value = ('')
+  }
+
+  return (
+    <section id="player">
+      <h2>Welcome { enterPlayerName ?? 'unknown entity'}</h2>
+      <p>
+        <input type="text" ref={playerName} />
+        <button onClick={handleClick}>Set Name</button>
+      </p>
+    </section>
+  );
+}
